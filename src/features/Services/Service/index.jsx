91d9953d292c90ac6eee6c7fc72528d0f03c8a3e
Corey Hutcheson <hutcheson.coreyj@gmail.com>
2019-01-18 14:28:36 -0600

    added hover styling to service panels

--- src/features/Services/Service/index.jsx
+++ src/features/Services/Service/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
 import "./style.scss";
 
 function Service({ title, desc }) {
@@ -9,10 +11,10 @@ function Service({ title, desc }) {
     .replace(/[ ]/g, "-");
 
   return (
-    <div className={`service service__${uniqueClass}`}>
+    <Link to={`${title}`} className={`service service__${uniqueClass}`}>
       <h3 className="service__title">{title}</h3>
       <p className="service__description">{desc}</p>
-    </div>
+    </Link>
   );
 }
 
