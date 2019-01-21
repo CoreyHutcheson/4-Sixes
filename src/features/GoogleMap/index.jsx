3e705376b5329f497a2ec15c4bba5b737362390c
Corey <hutcheson.coreyJ@gmail.com>
2019-01-20 23:47:16 -0600

    added initial google map component

---
+++ src/features/GoogleMap/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./style.scss";
+
+function GoogleMap({}) {
+  return <div>I'm a google map</div>;
+}
+
+GoogleMap.propTypes = {};
+
+export default GoogleMap;
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import Carousel from "features/Carousel";
 import Services from "features/Services";
+import GoogleMap from "features/GoogleMap";
 
 const Home = () => (
   <div className="page-home">
     <Carousel />
     <Services />
+    <GoogleMap />
   </div>
 );
 
