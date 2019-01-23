31f48a48417bde6a0ad57bb2ecec7fa4c675a504
Corey Hutcheson <hutcheson.coreyj@gmail.com>
2019-01-22 19:26:51 -0600

    removed style.scss for GoogleMap component / styled about link in footer on hover

--- src/features/GoogleMap/index.jsx
+++ src/features/GoogleMap/index.jsx
@@ -1,16 +1,9 @@
 import React from "react";
 
 import map from "assets/google-maps-image.JPG";
-import "./style.scss";
 
 function GoogleMap() {
-  return (
-    <img
-      src={map}
-      className="map__img"
-      alt="Google Map showing location of store"
-    />
-  );
+  return <img src={map} alt="Google Map showing location of store" />;
 }
 
 export default GoogleMap;
