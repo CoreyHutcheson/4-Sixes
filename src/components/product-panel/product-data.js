46b8dbf0362d4dcc2720d117c2ef1a1f7e26b504
Corey <hutcheson.coreyJ@gmail.com>
2019-03-10 15:31:44 -0500

    createProductObject function created to create the product objects

--- src/components/product-panel/product-data.js
+++ src/components/product-panel/product-data.js
@@ -22,84 +22,95 @@ const images = importAll(
   require.context("assets/images/products", false, /\.(png|jpe?g|svg)$/)
 );
 
+const createProductObject = ([name, img, type, desc]) => ({
+  name,
+  img,
+  type,
+  desc
+});
+
 /**
  * Array of objects
  * {name, img, type, desc}
  */
 const productList = [
-  {
-    name: "Grey Goose",
-    img: images["grey-goose"],
-    type: "Vodka",
-    desc:
-      "A Single-origin French vodka, traceable from field to bottle for a naturally rich and full-bodied taste."
-  },
-  {
-    name: "Jack Daniels",
-    img: images["jack-daniels"],
-    type: "Whiskey",
-    desc:
-      "Known for its smooth, sweet and mild palate that opens with flavors of caramel, complemented by hints of spice, nuts and a touch of smoke"
-  },
-  {
-    name: "Amber Falls",
-    img: images["amber-falls"],
-    type: "Wine",
-    desc: "Superb local wine from the vineyards in Hampshire, TN."
-  },
-  {
-    name: "Captain Morgan",
-    img: images["captain-morgan"],
-    type: "Rum",
-    desc: "To Life, Love and Loot."
-  },
-  {
-    name: "Balvenie",
-    img: images["balvenie"],
-    type: "Scotch",
-    desc:
-      "A 12 year old single malt whisky which gains it's distinctive character from being matured in two wood types."
-  },
-  {
-    name: "Belle Meade",
-    img: images["belle-meade"],
-    type: "Bourbon",
-    desc:
-      "Prior to Prohibition, Belle Meade Bourbon was the choice of the discerning whiskey lover. The same is true today"
-  },
-  {
-    name: "Crown Royal Peach",
-    img: images["crown-royal-peach"],
-    type: "Whisky",
-    desc: "New Limited Edition. Bringing juicy sweetness to your summer season."
-  },
-  {
-    name: "Crystal Head Vodka",
-    img: images["crystal-head-vodka"],
-    type: "Vodka",
-    desc: "Some of the smoothiest vodka in one of the slickest bottles!"
-  },
-  {
-    name: "Heavens Door",
-    img: images["heavens-door"],
-    type: "Whiskey",
-    desc:
-      "An award-winning collection of handcrafted American whiskey developed in partnership with Bob Dylan."
-  },
-  {
-    name: "Jack Daniels Gold",
-    img: images["jack-daniels-gold"],
-    type: "Whiskey",
-    desc:
-      "Crafted to the same uncompromising standards as Old No. 7, but then twice charcoal mellowed to be luxuriously smooth."
-  },
-  {
-    name: "Sangre de Vida",
-    img: images["vida-tequila-heart"],
-    type: "Tequila",
-    desc:
-      "Award winning, triple distilled, 100% blue webber agave tequila presented in a stunning anatomically correct glass heart bottle."
-  }
+  // Grey Goose
+  createProductObject([
+    "Grey Goose",
+    images["grey-goose"],
+    "Vodka",
+    "A Single-origin French vodka, traceable from field to bottle for a naturally rich and full-bodied taste."
+  ]),
+  // Jack Daniels
+  createProductObject([
+    "Jack Daniels",
+    images["jack-daniels"],
+    "Whiskey",
+    "Known for its smooth, sweet and mild palate that opens with flavors of caramel, complemented by hints of spice, nuts and a touch of smoke"
+  ]),
+  // Amber Falls
+  createProductObject([
+    "Amber Falls",
+    images["amber-falls"],
+    "Wine",
+    "Superb local wine from the vineyards in Hampshire, TN."
+  ]),
+  // Captain Morgan
+  createProductObject([
+    "Captain Morgan",
+    images["captain-morgan"],
+    "Rum",
+    "To Life, Love and Loot."
+  ]),
+  // Balvenie
+  createProductObject([
+    "Balvenie",
+    images["balvenie"],
+    "Scotch",
+    "A 12 year old single malt whisky which gains it's distinctive character from being matured in two wood types."
+  ]),
+  // Belle Meade
+  createProductObject([
+    "Belle Meade",
+    images["belle-meade"],
+    "Bourbon",
+    "Prior to Prohibition, Belle Meade Bourbon was the choice of the discerning whiskey lover. The same is true today"
+  ]),
+  // Crown Royal Peach
+  createProductObject([
+    "Crown Royal Peach",
+    images["crown-royal-peach"],
+    "Whisky",
+    "New Limited Edition. Bringing juicy sweetness to your summer season."
+  ]),
+  // Crystal Head Vodka
+  createProductObject([
+    "Crystal Head Vodka",
+    images["crystal-head-vodka"],
+    "Vodka",
+    "Some of the smoothiest vodka in one of the slickest bottles!"
+  ]),
+  // Heavens Door
+  createProductObject([
+    "Heavens Door",
+    images["heavens-door"],
+    "Whiskey",
+    "An award-winning collection of handcrafted American whiskey developed in partnership with Bob Dylan."
+  ]),
+  // Jack Daniels Gold
+  createProductObject([
+    "Jack Daniels Gold",
+    images["jack-daniels-gold"],
+    "Whiskey",
+    "Crafted to the same uncompromising standards as Old No. 7, but then twice charcoal mellowed to be luxuriously smooth."
+  ]),
+  // Sangre de Vida
+  createProductObject([
+    "Sangre de Vida",
+    images["vida-tequila-heart"],
+    "Tequila",
+    "Award winning, triple distilled, 100% blue webber agave tequila presented in a stunning anatomically correct glass heart bottle."
+  ])
 ];
 
 export default productList;
