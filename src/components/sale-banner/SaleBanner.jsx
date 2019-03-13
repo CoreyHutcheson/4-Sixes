3082152dcf0d3e52e28bf52f8f9c133777e2d816
Corey <hutcheson.coreyJ@gmail.com>
2019-03-13 12:05:46 -0500

    begun styling sales banner

--- src/components/sale-banner/SaleBanner.jsx
+++ src/components/sale-banner/SaleBanner.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./style.scss";
 
 function SaleBanner() {
-  return <div>Sale Banner</div>;
+  return (
+    <div className="sales-banner">
+      <Link to="/about" className="sales-banner__link">
+        SAVE $! Check out our Sales!
+      </Link>
+    </div>
+  );
 }
 
 export default SaleBanner;
