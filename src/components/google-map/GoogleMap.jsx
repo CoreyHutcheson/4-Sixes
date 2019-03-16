5e37578bd5c1c3f2386052e26a04ed37d2296086
Corey Hutcheson <hutcheson.coreyj@gmail.com>
2019-03-16 12:49:00 -0500

    removed google map image, and GoogleMap component

--- src/components/google-map/GoogleMap.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import map from "src/assets/images/google-maps-image.jpg";
-
-function GoogleMap() {
-  return <img src={map} alt="Google Map showing location of store" />;
-}
-
-export default GoogleMap;
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 
 import PageWrapper from "src/components/page-wrapper";
-import GoogleMap from "src/components/google-map";
 import "src/utils/scss/page-styles/about.scss";
 
 const About = () => (
   <PageWrapper>
     <div className="page-about">
       <h2 className="page-about__title">About</h2>
-      <div className="page-about__map">
-        <GoogleMap />
-      </div>
     </div>
   </PageWrapper>
 );
