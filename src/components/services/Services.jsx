fe08dc23be4f09908941157e234d3080908294aa
Corey Hutcheson <hutcheson.coreyj@gmail.com>
2019-03-16 09:36:01 -0500

    updated Services component

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -9,19 +9,19 @@ function Services() {
       key: "History",
       title: "History",
       desc:
-        "It was all in the cards! Discover the story behind your favorite liquor store."
+        "It was all in the cards! Discover the story behind your favorite liquor store.",
     },
     {
       key: "Products",
       title: "Products",
       desc:
-        "What's new? What's trending? What are you buying later? Find out here!"
+        "What's new? What's trending? What are you buying later? Find out here!",
     },
     {
       key: "About",
       title: "About",
-      desc: "Want to contact us? Can't find the store? Well here's how!"
-    }
+      desc: "Want to contact us? Can't find the store? Well here's how!",
+    },
   ];
 
   return (
