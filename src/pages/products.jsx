c2c11b6724cbc215f86c935161024c8673647d71
Corey <hutcheson.coreyJ@gmail.com>
2019-03-16 20:41:19 -0500

    Adds New and Products headers, and pulls in product panel based on the newProduct property

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -7,9 +7,16 @@ import "src/utils/scss/page-styles/products.scss";
 const Products = () => (
   <PageWrapper>
     <div className="page-products">
-      <div className="page-products__products-grid">
-        <ProductPanel />
+      <div className="page-products__header">New</div>
+      <div className="page-products__products-grid page-products__new-products">
+        <ProductPanel newProducts={true} />
       </div>
+
+      <div className="page-products__header">Products</div>
+      <div className="page-products__products-grid">
+        <ProductPanel newProducts={false} />
+      </div>
+
       <div className="page-products__more">Plus More!</div>
     </div>
   </PageWrapper>
