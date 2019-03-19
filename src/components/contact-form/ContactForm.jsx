a4f4cef81c17c3337bd9cc8e957ffbe465c0201f
Corey Hutcheson <hutcheson.coreyj@gmail.com>
2019-03-19 17:58:39 -0500

    styled ContactForm component

--- src/components/contact-form/ContactForm.jsx
+++ src/components/contact-form/ContactForm.jsx
@@ -6,33 +6,50 @@ function ContactForm() {
   return (
     <form
       name="contact"
-      className="contact-form"
+      className="form"
       method="post"
       data-netlify="true"
       data-netlify-honeypot="bot-field"
     >
       <input type="hidden" name="bot-field" />
 
-      <div className="">
+      <div className="form__field-container">
         <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" />
+        <input
+          type="text"
+          className="form__field"
+          name="name"
+          id="name"
+          placeholder="John Smith"
+        />
       </div>
-      <div className="">
+
+      <div className="form__field-container">
         <label htmlFor="email">Email</label>
-        <input type="text" name="email" id="email" />
+        <input
+          type="text"
+          className="form__field"
+          name="email"
+          id="email"
+          placeholder="john_smith@gmail.com"
+        />
       </div>
-      <div className="">
+
+      <div className="form__field-container form__field-container--full">
         <label htmlFor="message">Message</label>
-        <textarea name="message" id="message" rows="6" />
+        <textarea
+          className="form__field--message"
+          name="message"
+          id="message"
+          rows="6"
+          placeholder="I'd like to inquiry about..."
+        />
       </div>
-      <ul className="">
-        <li>
-          <input type="submit" value="Send Message" className="special" />
-        </li>
-        <li>
-          <input type="reset" value="Clear" />
-        </li>
-      </ul>
+
+      <div className="form__button-container">
+        <input type="submit" value="Submit" className="form__button" />
+        <input type="reset" value="Clear" className="form__button" />
+      </div>
     </form>
   );
 }
