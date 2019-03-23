d0f16179ae528e7ece8a300a67ee94847e495189
Corey Hutcheson <hutcheson.coreyj@gmail.com>
2019-03-23 10:15:45 -0500

    Added gatsby-source-filesystem and gatsby-transformer-json plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-root-import"],
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-root-import",
+    "gatsby-transformer-json",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`,
+      },
+    },
+  ],
 };
