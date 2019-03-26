dd1ea630e27caf4214709bada1e88a688e01185b
Corey <hutcheson.coreyJ@gmail.com>
2019-03-25 23:13:34 -0500

    Refactored Services component to query services.json for data to create Service panels

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,33 +1,28 @@
 import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
 
 import Service from "./service";
 import "./style.scss";
 
 function Services() {
-  const services = [
-    {
-      key: "History",
-      title: "History",
-      desc:
-        "It was all in the cards! Discover the story behind your favorite liquor store.",
-    },
-    {
-      key: "Products",
-      title: "Products",
-      desc:
-        "What's new? What's trending? What are you buying later? Find out here!",
-    },
-    {
-      key: "About",
-      title: "About",
-      desc: "Want to contact us? Can't find the store? Well here's how!",
-    },
-  ];
+  const data = useStaticQuery(graphql`
+    query ServicesQuery {
+      allServicesJson {
+        edges {
+          node {
+            key
+            title
+            desc
+          }
+        }
+      }
+    }
+  `).allServicesJson.edges;
 
   return (
     <div className="services">
-      {services.map(item => (
-        <Service key={item.key} title={item.title} desc={item.desc} />
+      {data.map(({ node }) => (
+        <Service key={node.key} title={node.title} desc={node.desc} />
       ))}
     </div>
   );
