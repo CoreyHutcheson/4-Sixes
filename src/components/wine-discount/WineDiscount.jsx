155ef98faa96edc963e76c4077f38d98e1591b1c
Corey <hutcheson.coreyJ@gmail.com>
2019-03-29 17:54:49 -0500

    created and added WineDiscount component to events page

---
+++ src/components/wine-discount/WineDiscount.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./style.scss";
+
+const WineDiscount = () => (
+  <section className="wine-discount">
+    <h2 className="wine-discount__header">Wine Tuesdays</h2>
+    <div className="wine-discount__content">
+      Every Tuesday we offer a 20% discount on all wines! So come in and enjoy
+      your favorite wine at an even cheaper price!
+    </div>
+  </section>
+);
+
+export default WineDiscount;
--- src/pages/events.jsx
+++ src/pages/events.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import PageWrapper from "src/components/page-wrapper";
 import FAQ from "src/components/faq";
 import EventSection from "src/components/event-section";
+import WineDiscount from "src/components/wine-discount";
 
 import "src/utils/scss/page-styles/events.scss";
 
@@ -11,6 +12,7 @@ const EventsPage = ({ data }) => {
     <PageWrapper>
       <div className="page-content page-events">
         <EventSection />
+        <WineDiscount />
         <FAQ />
       </div>
     </PageWrapper>
