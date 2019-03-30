d24c92d0b1384f2344ac943c8cf337866836b28f
Corey Hutcheson <hutcheson.coreyj@gmail.com>
2019-03-30 12:28:36 -0500

    created HistoryPanel component and added it to the history.js page

---
+++ src/components/history-panel/HistoryPanel.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./style.scss";
+
+const HistoryPanel = ({ img, altText, text }) => (
+  <>
+    <div className="page-history__img-container">
+      <img src={img} alt={altText} />
+    </div>
+
+    <p className="page-history__text">{text}</p>
+  </>
+);
+
+HistoryPanel.propTypes = {
+  img: PropTypes.string.isRequired,
+  altText: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
+
+export default HistoryPanel;
--- src/pages/history.jsx
+++ src/pages/history.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import PageWrapper from "src/components/page-wrapper";
+import HistoryPanel from "src/components/history-panel";
 import cards from "src/assets/images/history/cards[sketch-1000x500].jpg";
 import board from "src/assets/images/history/board[sketch-1000x500].jpg";
 import sign from "src/assets/images/history/sign[sketch-1000x500].jpg";
@@ -10,38 +11,21 @@ import "src/utils/scss/page-styles/history.scss";
 const History = () => (
   <PageWrapper>
     <div className="page-content page-history">
-      <div className="page-history__img-container">
-        <img src={cards} alt="Pocket sixes cards." />
-      </div>
-
-      <p className="page-history__text">
-        Card Text Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse
-        error magni ipsa sed necessitatibus repellendus hic aspernatur! At nemo,
-        error provident eveniet dolores adipisci officia, fugiat dolore
-        accusamus suscipit eligendi.
-      </p>
-
-      <div className="page-history__img-container page-history__board">
-        <img src={board} alt="Board showing two sixes to make quad sixes." />
-      </div>
-
-      <p className="page-history__text">
-        Board Text Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Corporis iusto soluta libero nihil totam quae sapiente exercitationem
-        delectus ad repudiandae vero, ratione, fugiat nulla aperiam iure aut
-        deserunt quasi assumenda!
-      </p>
-
-      <div className="page-history__img-container">
-        <img src={sign} alt="Four sixes sign outside of store." />
-      </div>
-
-      <p className="page-history__text">
-        Sign Text Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Perspiciatis nulla officia odit a ea, minus blanditiis voluptate dolor
-        molestias. Blanditiis error ex ipsam quo praesentium officia voluptatem
-        illum veniam est.
-      </p>
+      <HistoryPanel
+        img={cards}
+        altText="Pocket sixes cards."
+        text="Card Text Lorem ipsum dolor"
+      />
+      <HistoryPanel
+        img={board}
+        altText="Board showing two sixes to make quad sixes."
+        text="Board Text Lorem ipsum dolor sit"
+      />
+      <HistoryPanel
+        img={sign}
+        altText="Four sixes sign outside of store."
+        text="Sign Text Lorem ipsum dolor sit amet"
+      />
     </div>
   </PageWrapper>
 );
