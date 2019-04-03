be0bd836258f7c7869284a1772c587c7c09f5a13
Corey <hutcheson.coreyJ@gmail.com>
2019-04-03 09:53:58 -0500

    ProductPanel component use custom wordpress hook

--- src/components/product-panel/ProductPanel.jsx
+++ src/components/product-panel/ProductPanel.jsx
@@ -1,47 +1,29 @@
 import React from "react";
-import { useStaticQuery, graphql } from "gatsby";
+
+import { useWordpressProducts } from "src/utils/js/custom-hooks/useWordpressProducts";
 import Img from "gatsby-image";
 
 import "./style.scss";
 
 const ProductPanel = ({ newProducts }) => {
-  const data = useStaticQuery(graphql`
-    query ProductQuery {
-      allProductsJson {
-        edges {
-          node {
-            key
-            name
-            type
-            desc
-            newProduct
-            img {
-              childImageSharp {
-                fluid {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  `).allProductsJson.edges;
+  const data = useWordpressProducts();
 
-  const products = data.filter(({ node }) => node.newProduct === newProducts);
+  const products = data.filter(({ node }) => {
+    return node.acf.new_product === newProducts;
+  });
 
   return products.map(({ node }) => {
     return (
-      <div key={node.key} className="product">
+      <div key={node.id} className="product">
         <Img
           className="product__img"
-          fluid={node.img.childImageSharp.fluid}
-          alt={node.name}
+          fluid={node.acf.image.localFile.childImageSharp.fluid}
+          alt={node.title}
         />
-        <div className="product__name">{node.name}</div>
-        <div className="product__type">{node.type}</div>
+        <div className="product__name">{node.title}</div>
+        <div className="product__type">{node.acf.type}</div>
         <div className="product__desc">
-          <p>{node.desc}</p>
+          <p>{node.acf.description}</p>
           <span>Description:</span>
         </div>
       </div>
