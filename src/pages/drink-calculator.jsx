b44c4c3ee29a07a7533a0dde1a43c546ef2cd8a6
Corey Hutcheson <hutcheson.coreyj@gmail.com>
2019-04-08 13:55:31 -0500

    created drink-calculator page

---
+++ src/pages/drink-calculator.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import PageWrapper from "src/components/page-wrapper";
+import DrinkCalculator from "src/components/drink-calculator";
+import "src/utils/styles/page-styles/drink-calculator.scss";
+
+const Home = () => {
+  return (
+    <PageWrapper>
+      <div className="page-content page-drink-calculator">
+        <h2>Drink Calculator</h2>
+        <p>
+          Planning an event? Let use help you decide how much you need to buy!
+        </p>
+        <DrinkCalculator />
+      </div>
+    </PageWrapper>
+  );
+};
+
+export default Home;
